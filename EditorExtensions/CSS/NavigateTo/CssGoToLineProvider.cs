7915548cff857c971b31b8cb73850e1f04cb2348
Buri Tamás <tamas.buri@gmail.com>
2014-09-18 21:46:14 +0200

    Updated references for VS 14 CTP 3

--- EditorExtensions/CSS/NavigateTo/CssGoToLineProvider.cs
+++ EditorExtensions/CSS/NavigateTo/CssGoToLineProvider.cs
@@ -11,7 +11,9 @@ using Microsoft.VisualStudio.PlatformUI;
 
 namespace MadsKristensen.EditorExtensions.Css
 {
-    internal sealed class CssGoToLineProvider : DisposableObject, INavigateToItemProvider
+    internal sealed class CssGoToLineProvider :    INavigateToItemProvider
+                                              //^^  DisposableObject, 
+
     {
         private sealed class Worker : IDisposable
         {
@@ -181,5 +183,40 @@ namespace MadsKristensen.EditorExtensions.Css
                 currentWorker.Cancel();
             }
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).          
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources. 
+        // ~CssGoToLineProvider() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
